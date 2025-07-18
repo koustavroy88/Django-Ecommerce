2d4cc0039e4cf136cded3879ce9c50a8a2b8240f
koustav <koustavroy88@gmail.com>
2025-07-19 02:56:57 +0300

    Added db password environment

--- ecom/settings.py
+++ ecom/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER':'postgres',
-        'PASSWORD':os.environ.get('DB_PASSWORD_YO'),
+        'PASSWORD':os.environ['DB_PASSWORD_YO'],
         'HOST':'hopper.proxy.rlwy.net',
         'PORT':'41246',
     }
